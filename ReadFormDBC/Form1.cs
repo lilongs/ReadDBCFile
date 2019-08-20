793fb32e868001810ee806413c6a0384493459c0
lilong <871977737@qq.com>
2019-08-20 15:40:38 +0800

    init

---
+++ ReadFormDBC/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Aptiv.DBCFiles;
+using Aptiv.DBCFiles.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadFormDBC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = file.FileName;
+            }
+            DBCFile dbcFile = new DBCFile(file.FileName, true);
+
+            Dictionary<string, Dictionary<string, List<string>>> Attributes = dbcFile.Attributes;
+            string FileName=dbcFile.Filename;
+            Dictionary<string, DBCMessage> messages =dbcFile.messages;
+            Dictionary<int, string> MsgSendTypes=dbcFile.MsgSendTypes;
+            Dictionary<string, int> NS_Lib = dbcFile.NS_Lib;
+            Dictionary<string, List<IDBCSignal>> SignalGroups = dbcFile.SignalGroups;
+        }
+    }
+}
